d2ce501c231ed57f0f1ddeb517841b65dc083d53
Artem-io <zana1artm.io@gmail.com>
2025-08-17 21:17:07 +0200

    First commit

---
+++ asteroid-api/src/main/java/org/example/asteroidapi/AsteroidApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.asteroidapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsteroidApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsteroidApiApplication.class, args);
+    }
+
+}
